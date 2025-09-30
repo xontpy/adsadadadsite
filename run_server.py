fe01a38ffc2db56b3880fd4d3747442cde4e013c
XONT <137187224+xontpy@users.noreply.github.com>
2025-09-30 07:52:18 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -2,6 +2,10 @@ import os
 import sys
 import uvicorn
 import multiprocessing
+
+# Add the script's directory to the Python path to resolve module not found errors.
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from server import app
 
 if __name__ == "__main__":
