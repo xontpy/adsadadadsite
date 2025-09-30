03c07f623b80cd8abb50535d65403f7f704c530e
XONT <137187224+xontpy@users.noreply.github.com>
2025-09-30 15:53:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -191,8 +191,8 @@ async function fetchUserData(token) {
 
         const payload = {
             channel: channel,
-            views: num_viewers,
-            duration: duration_minutes
+            num_viewers: num_viewers,
+            duration_minutes: duration_minutes
         };
 
         try {
